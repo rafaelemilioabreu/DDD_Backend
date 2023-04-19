7833b2df48a2ec208f3f24a91ff07b7fcd707503
Rafael Emilio <rafaelemilioabreuvaldez@gmail.com>
2023-04-19 09:44:34 -0400

    chore: Repository Interface

---
+++ Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+        Task<Book> GetBookByIdAsync(int id);
+        Task CreateBookAsync(Book book);
+        Task UpdateBookAsync(int id, Book book);
+        Task DeleteBookAsync(int id);
+    }
+}
