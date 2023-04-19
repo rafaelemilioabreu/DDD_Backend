fe12272a4de123a1fd0539c984428e3d77cddd6b
Rafael Emilio <rafaelemilioabreuvaldez@gmail.com>
2023-04-19 09:21:49 -0400

    feat: Domain

---
+++ Domain/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
